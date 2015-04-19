270e5db8e86a0341b9f2b7511198e04216147f48
Thomas Decaux <ebuildy@gmail.com>
2015-04-19 17:06:51 +0200

    Fix deploy

--- grunt/exec.js
+++ grunt/exec.js
@@ -1,4 +1,4 @@
 module.exports = {
     git_push: "git add . --all && git commit -am \"Work work\" && git push",
-    deploy: 'rsync -az --exclude=".DS_Store" -e "ssh -p 666" ./dist/ root@91.121.26.12:/home/bizlunch/app/webapp/'
+    deploy: 'rsync -az --exclude=".DS_Store" ./app/dist/ k-public.cloudapp.net:/home/kinoulink/app/public'
 };
